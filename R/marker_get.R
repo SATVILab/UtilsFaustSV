8ef1582999c432b12c1cdfd25dcf8aa451bb905a
Sian Wood <sianewood@gmail.com>
2026-08-25 15:40:46 +0200

    Remove logic error in faust_merger_get_usage

--- R/marker_get.R
+++ R/marker_get.R
@@ -15,31 +15,9 @@ faust_marker_get_usage <- function(project_path) {
     dir_metadata, "scampClusterNames.rds"
   ))[1]
 
-  # all channels considered
-  active_chnl_vec <- readRDS(file.path(dir_metadata, "activeChannels.rds"))
+  matches <- stringr::str_match_all(cluster_name, "([^~]+)~\\d+~(\\d+)~")[[1]]
 
-  # filter all channels considered to get all channels used
-  clustered_chnl_vec <- NULL
-  for (x in active_chnl_vec) {
-    if (stringr::str_detect(cluster_name, x)) {
-      clustered_chnl_vec <- c(clustered_chnl_vec, x)
-    }
-  }
-
-  # get observed levels for each channel used
-  n_level_vec <- stats::setNames(
-    rep(NA, length(clustered_chnl_vec)), clustered_chnl_vec
-  )
-  for (i in seq_along(clustered_chnl_vec)) {
-    n_level_loc <- stringr::str_locate(
-      cluster_name, clustered_chnl_vec[i]
-    )[, "end"][[1]] + 4
-
-    n_level_vec[i] <- as.numeric(
-      stringr::str_sub(cluster_name, n_level_loc, n_level_loc)
-    )
-  }
-  n_level_vec
+  stats::setNames(as.numeric(matches[, 3]), matches[, 2])
 }
 
 faust_count_get <- function(project_path, exhaustive = FALSE) {
